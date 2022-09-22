794ed46c8011a3ce7338c5d0668e53f631d80b34
TandeepSingh1 <113154389+TandeepSingh1@users.noreply.github.com>
2022-09-22 15:24:19 -0300

    Commit 1
    Commit 1

---
+++ task0.py
@@ -0,0 +1,10 @@
+#Reference from https://medium.com/@GalarnykMichael/python-basics-8-fizzbuzz-441e97c6c767
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print('FizzBuzz')
+    elif num % 3 == 0:
+        print('Fizz')
+    elif num % 5 == 0:
+        print('Buzz')
+    else:
+        print(num)
